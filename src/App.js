4d974fba0ecf9e16c366e9605d7f5d9c329bb55b
roshniraut <roshniraut2000@gmail.com>
2021-05-20 12:38:01 +0530

    edit react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hii
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is Home page
+      </Route>
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+      <Route>
+        This is 404 Page
+      </Route>
+    </Switch>
   );
 }
 
